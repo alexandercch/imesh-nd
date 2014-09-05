f71ddaeb9c625891f6ee889b80cd621486af9b1c
alex <alexander.cc.h@gmail.com>
2014-09-05 12:20:30 -0500

    funcion fibonacci añadida

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 using namespace std;
-
+int fib(int n){
+	if (n<2) return n;
+	return fib(n-1) + fib(n-2);
+}
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << fib(10) << endl;
     return 0;
 }
